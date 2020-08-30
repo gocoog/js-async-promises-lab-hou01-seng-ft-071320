60733f5dc2f580d651cb6e399a334ba0fcb725da
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-08-30 15:22:24 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,45 @@ const questions = [
 ]
 
 let question;
+
+let appendQuestion = (question) => {
+  let container = document.querySelector('.question-container')
+  container.innerHTML = question.questionText
+}
+
+let askQuestionThen = (time) => {
+  question = questions[0];
+  appendQuestion(question)
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      resolve()
+    }, time)
+  })
+}
+
+let removeQuestion = () => {
+  return new Promise((resolve, reject) => {
+    let container = document.querySelector('.question-container')
+    container.innerHTML = ''
+  })
+}
+
+let askQuestionThenRemoveQuestion = (time) => {
+  return askQuestionThen(time)
+  .then(removeQuestion)
+}
+
+let trueAndFalseButtons = () => {
+  return document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+let toggleTrueAndFalseButtons = () => {
+  document.querySelector('div.btn.red').classList.toggle("hide")
+  document.querySelector('div.btn.green').classList.toggle("hide")
+}
+
+let displayQuestionOnClick = () => {
+  let btn = document.querySelector('.waves-effect')
+  btn.addEventListener('click', appendQuestion(question));
+  toggleTrueAndFalseButtons();
+}
